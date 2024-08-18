aa29353c8b623b7651e506b51918c40f7dcf50b0
ntk132 <ngtkie1302@gmail.com>
2024-08-18 10:41:57 +0700

    [feat] handle file metadata api

--- boilerplate-project-filemetadata-main/index.js
+++ boilerplate-project-filemetadata-main/index.js
@@ -12,6 +12,32 @@ app.get('/', function (req, res) {
 });
 
 
+const bodyParser= require('body-parser')
+const multer = require('multer');
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage });
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.send({name: file.originalname, type: file.mimetype, size: file.size});
+});
 
 
 const port = process.env.PORT || 3000;
